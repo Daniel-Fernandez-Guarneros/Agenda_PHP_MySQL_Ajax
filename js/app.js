0d7549333c0a66ae5bf2dd844f0e65d0d769de7a
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-07-09 23:51:34 -0500

    009 Creando una Notficación cuando el Formulario esta vacio

--- js/app.js
+++ js/app.js
@@ -16,8 +16,33 @@ function leerFormulario(e) {
           telefono = document.querySelector('#telefono').value;
     
     if(nombre === '' || empresa === '' || telefono === '') {
-        console.log('Los campos estan vacios')
+
+        mostrarNotificacion('Todos los campos son obligatorios', 'error');
     } else {
         console.log('Tiene algo');
     }
+}
+
+
+// Notificacion que aparece en pantalla
+function mostrarNotificacion(mensaje, clase) {
+    const notificacion = document.createElement('div');
+    notificacion.classList.add(clase, 'notificacion', 'sombra');
+    notificacion.textContent = mensaje;
+
+    // Formulario
+    formularioContactos.insertBefore(notificacion, document.querySelector('form legend'));
+
+    // Mostrar y ocultar la notificacion
+    setTimeout(() => {
+         notificacion.classList.add('visible');
+
+         setTimeout(() => {
+              notificacion.classList.remove('visible');     
+              setTimeout(() => {
+                   notificacion.remove();
+              }, 500)
+         }, 3000);
+    }, 100);
+
 }
