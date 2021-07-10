86f0c74bb95dced38b5920e70c606fe291f67f8f
Daniel Fernandez Guarneros <danierse63@gmail.com>
2021-07-09 20:19:51 -0500

    002 Creando el Formulario

--- index.php
+++ index.php
@@ -5,5 +5,29 @@
      <h1>Agenda de Contactos</h1>
 </div>
 
+<div class="bg-amarillo contenedor sombra">
+    <form id="contacto" action="#">
+        <legend>Añade un contacto <span>Todos los campos son obligatorios</span></legend>
+
+        <div class="campos">
+            <div class="campo">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="Nombre Contacto" id="nombre">
+            </div>
+            <div class="campo">
+                <label for="empresa">Empresa:</label>
+                <input type="text" placeholder="Nombre Empresa" id="empresa">
+            </div>
+            <div class="campo">
+                <label for="nombre">Teléfono:</label>
+                <input type="tel" placeholder="Nombre Contacto" id="nombre">
+            </div>
+            <div class="campo enviar">
+                <input type="submit" value="Añadir">
+            </div>
+        </div>
+    </form>
+</div>
+
 
 <?php include 'inc/layout/footer.php'; ?>
